137347dcf48d2aea2c19617374ddcfea35401d21
Nataliq <nat.g.dimitrova@gmail.com>
2024-02-26 11:51:54 +0200

    implemented history modal :D

---
+++ history/seeder.js
@@ -0,0 +1,22 @@
+let exampleActive = [
+    {
+        "parkingSpaceId": "A1",
+        "duration": "02:00:00",
+        "startTime": "26.02.2024, 11:00"
+    },
+    {
+        "parkingSpaceId": "A2",
+        "duration": "02:00:00",
+        "startTime": "26.02.2024, 11:00"
+    },
+    {
+        "parkingSpaceId": "A3",
+        "duration": "02:00:00",
+        "startTime": "26.02.2024, 11:00"
+    },
+    {
+        "parkingSpaceId": "A4",
+        "duration": "02:00:00",
+        "startTime": "26.02.2024, 11:00"
+    }
+]
